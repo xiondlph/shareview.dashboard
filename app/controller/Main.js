2ee5875fe1d958f7b465c3cf1b06ed00c1e19df7
Ismailov Shuhrat <admin@ismax.ru>
2016-12-13 18:43:33 +0300

    Обновление Main.js

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -2,12 +2,86 @@ Ext.define('Admin.controller.Main', {
     extend: 'Ext.app.Controller',
 
     onLaunch: function () {
+        Admin.Overlay = this.showOverlay;
+
+        this.loadProfile();
+    },
+
+    onAuth: function (userData) {
+        Ext.Viewport.remove('login');
+
+        this.showDashboard(userData);
+    },
+    
+    loadProfile: function () {
+        var me = this,
+            overlay;
+
+        Ext.Ajax.request({
+            url: 'resources/data/forbidden.json' //'/api/profile'
+        }).then(function(response, opts) {
+            var data = Ext.decode(response.responseText);
+
+            if (data.success) {
+                me.showDashboard(data);
+            } else {
+                me.showLoginForm();
+            }
+        }, function (response, opts) {
+            Admin.Overlay();
+        });
+    },
+
+    showLoginForm: function () {
         var Login = Ext.create('Admin.view.authentication.Login', {
-            shu: 'shu'
+            itemId: 'login',
+            listeners: {
+                auth: {
+                    scope: this,
+                    fn: this.onAuth
+                }
+            }
         });
         Ext.Viewport.add(Login);
-        // Ext.Viewport.add({
-        //     xtype: 'admin.dashboard'
-        // });
+    },
+
+    showDashboard: function (userData) {
+        Ext.Viewport.add({
+            xtype: 'admin.dashboard'
+        });
+    },
+
+    /**
+     * Перехват ошибок аякс запросов
+     */
+    setAjaxSettings: function () {
+        Ext.Ajax.on('requestexception', function(connection, response, options) {
+            if (response.status === 403) {
+                // reload
+                return;
+            }
+
+            Admin.Overlay();
+        }, this);
+    },
+
+    showOverlay: function (message) {
+        var me = this,
+            msg = message || 'Действие временно недоступно.<br />Попробуйте повторить позже!',
+            overlay = Ext.create('Ext.ActionSheet', {
+                items: [{
+                    xtype: 'label',
+                    html: msg
+                }, {
+                    text: 'OK',
+                    scope: me,
+                    handler: function() {
+                        overlay.hide();
+                    }
+                }]
+            });
+
+        Ext.Viewport.add(overlay);
+        overlay.show();
     }
 });
